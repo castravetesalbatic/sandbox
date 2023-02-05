1cbd18988729f658f62b2dfc4a44eb8d36427960
castravetesalbatic <97475110+castravetesalbatic@users.noreply.github.com>
2023-02-05 09:19:26 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "Create your account:";
+
+ReactDOM.render(
+  <div>
+    <h1>{title}</h1>
+  </div>,
+  document.getElementById("root")
+);
